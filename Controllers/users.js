0747dc0c46ebbb54ce673f9d2bc2772e184aadae
matthewleemle <matthewleemle@gmail.com>
2019-10-24 00:37:32 -0700

    update

---
+++ Controllers/users.js
@@ -0,0 +1,84 @@
+require('dotenv').config({path: __dirname + '/process.env'})
+database = require("../database");
+var db = database.getdb();
+let jwt = require("jsonwebtoken");
+let middleware = require("../middleware");
+
+async function loginUser(req,res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var data = {
+      "username": username,
+      "password":password,
+  }
+
+  try {
+    console.log({username:data.username,password:data.password});
+    const result = await db.collection('details').findOne({username: data.username, password: data.password});
+
+    if (result){
+      let token = jwt.sign({username: data.username},
+        process.env.SECRET,
+        {
+          expiresIn: '24h'
+        }
+      );
+
+    res.status(200).json({
+      success: true,
+      message: 'Authentication successful!',
+      token: token,
+      id: result._id
+      })
+    console.log("Successful login.");
+    }
+
+  else{
+    res.status(404).send("Failed login");
+    }
+  }
+  catch (err){
+    console.log(err);
+    res.status(404).send("Failed retrieve");
+  };
+}
+
+
+function signupUser(req,res){
+    var name = req.body.name;
+    var username = req.body.username;
+    var email =req.body.email;
+    var pass = req.body.password;
+    var phone =req.body.phone;
+
+    var data = {
+      "name": name,
+      "username":username,
+      "email":email,
+      "password":pass,
+      "phone":phone
+      }
+
+    db.collection('details').insertOne(data,function(err, collection){
+      if (err) throw err;
+      console.log("Record inserted Successfully");
+      res.status(200).send("Successful signup");
+    });
+}
+
+async function userInfo(req,res){
+  const result = await db.collection('details').findOne({username: req.decoded.username});
+  if(req.params.id==result._id){
+      res.status(200).send(result);
+  }
+  else{
+    res.status(400).send("Access not allowed.");
+  }
+
+
+}
+
+module.exports = {
+  signupUser,loginUser,userInfo
+}
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-require("dotenv").config()
+require('dotenv').config({path: __dirname + '/process.env'})
+const database = require("./database");
+database.connect();
+const user = require("./Controllers/users");
+var express=require("express");
+var bodyParser=require("body-parser");
+let jwt = require("jsonwebtoken");
+let middleware = require("./middleware");
+const port = process.env.PORT || 3000;
 
-var express = require("express");
-var bodyParser = require("body-parser");
-
-var app = express();
-var port = process.env.PORT;
-
+var app=express()
+app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
     extended: true
-  })
-);
+}));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post("/signup", user.signupUser);
+app.post("/login", user.loginUser);
+app.get("/users/:id",middleware.checkToken,user.userInfo);
 
-app.listen(port, () => console.log(`UHN Backend Server listening on port ${port}!`))
+app.listen(port, function(){
+  console.log("Server is running");
+});
