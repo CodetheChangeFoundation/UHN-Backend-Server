4f55438d89b5a90c47d5784e8e4e812211449ebf
matthewleemle <matthewleemle@gmail.com>
2019-11-06 13:48:29 -0800

    update

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({path: __dirname + "/process.env"});
 let jwt = require("jsonwebtoken");
 
 let checkToken = (req, res, next) => {
--- testing.js
+++
@@ -1,6 +0,0 @@
-var bcrypt = require("bcrypt");
-const saltRounds = 10;
-var password = "1234"
-var hash = bcrypt.hashSync(password, saltRounds);
-
-console.log(hash);
