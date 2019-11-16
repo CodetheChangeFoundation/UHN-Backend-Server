839d41071adc631760abf9d55e81f408fc1bf6e7
Manu Mahadevan Koipallil <manukoipallil@yahoo.com>
2019-11-15 21:20:02 -0800

    Update user.js

--- Models/user.js
+++ Models/user.js
@@ -1,6 +1,7 @@
 var database = require("../database");
 var mongoose = database.getmongoose();
 var Schema = mongoose.Schema;
+var logSchema = mongoose.Schema;
 
 const model = mongoose.model("users", new Schema({
   username: String,
@@ -8,8 +9,12 @@ const model = mongoose.model("users", new Schema({
   email: String,
   phone: String
 }));
-
+const log_model = mongoose.model("users", new Schema({
+  log: LOG
+}));
+//I think LOG is a file format. It will incluethe time and 
 module.exports = {
-  model
-}
+  model,
+  log_model
 
+}
