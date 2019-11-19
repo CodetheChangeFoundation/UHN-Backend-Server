a65189f5dec854751bff6d36f9e28731190af15b
Matthew <matthewleemle@gmail.com>
2019-11-19 00:01:33 -0800

    fix env

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: __dirname + '/../process.env'});
+require('dotenv').config({path: __dirname + '/.env'});
 const database = require("./database");
 database.connect();
 const user = require("./Controllers/user");
