24218cce255973cf4041eb41a70159bb9ccf4512
Manu Mahadevan Koipallil <manukoipallil@yahoo.com>
2019-11-23 12:36:19 -0800

    Code schanges for logging
    Code for the log collection model. Still need to change user.js file under controllers

--- Models/user.js
+++ Models/user.js
@@ -1,7 +1,6 @@
 var database = require("../database");
 var mongoose = database.getmongoose();
 var Schema = mongoose.Schema;
-var logSchema = mongoose.Schema;
 
 const model = mongoose.model("users", new Schema({
   username: String,
@@ -9,12 +8,7 @@ const model = mongoose.model("users", new Schema({
   email: String,
   phone: String
 }));
-const log_model = mongoose.model("users", new Schema({
-  log: LOG
-}));
-//I think LOG is a file format. It will incluethe time and 
-module.exports = {
-  model,
-  log_model
 
+module.exports = {
+  model
 }
