2f92a735f7137ea21defa80838f75e6fbfa57269
Asiapenguin <nicholaskong@live.ca>
2019-12-20 12:02:36 -0800

    Formatting

--- database.js
+++ database.js
@@ -1,26 +1,27 @@
 const mongoose = require("mongoose");
 var db = null;
-const { check, validationResult } = require("express-validator");
 
-function connect(){
-  mongoose.connect("mongodb://localhost/uhn-database",{useNewUrlParser:true});
+function connect() {
+  mongoose.connect("mongodb://localhost/uhn-database", {
+    useNewUrlParser: true
+  });
   db = mongoose.connection;
   db.on("error", console.log.bind(console, "connection error"));
-  db.once("open", function(callback){
-      console.log("connected to database");
-  })
+  db.once("open", function(callback) {
+    console.log("connected to database");
+  });
 }
 
-function getdb(){
+function getdb() {
   return db;
 }
 
-function getmongoose(){
+function getmongoose() {
   return mongoose;
 }
 
 module.exports = {
-  connect : connect,
-  getdb : getdb,
-  getmongoose: getmongoose,
-}
+  connect: connect,
+  getdb: getdb,
+  getmongoose: getmongoose
+};
