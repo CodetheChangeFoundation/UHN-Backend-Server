f6baee12d4093b9b8661b4c5df09da2b438b811d
andrchoi <andrew.tt.choi@gmail.com>
2020-02-04 23:09:17 -0800

    BS-11: revised database schema

--- data.sql
+++ data.sql
@@ -7,8 +7,7 @@ DROP TABLE Users;
 -- Table of Users
 CREATE TABLE Users (
     ID serial PRIMARY KEY,
-    Username varchar,
-    Email varchar UNIQUE,
+    Username varchar UNIQUE,
     LastLogin timestamptz
 );
 
@@ -17,7 +16,8 @@ CREATE TABLE AlarmLog (
     ID serial PRIMARY KEY,
     UserID Integer NOT NULL,
     AlarmStart timestamptz NOT NULL,
-    AlarmTimeout timestamptz NOT NULL,
+    AlarmEnd timestamptz,
+    AlarmSent Boolean,
     FOREIGN KEY (UserID) REFERENCES Users(ID) ON DELETE CASCADE
 );
 
@@ -50,13 +50,14 @@ CREATE TABLE TreatmentLog (
 );
 
 /* Uncomment section to fill database with example data
-INSERT INTO Users (Username, Email, LastLogin) VALUES ('John', 'john@example.com', NOW());
-INSERT INTO Users (Username, Email, LastLogin) VALUES ('Alice', 'alice@test.com', '2020-01-30 09:45:00-05');
-INSERT INTO Users (Username, Email, LastLogin) VALUES ('Bob', 'bob@test.com', '2020-02-01 15:05:00-08');
-INSERT INTO Users (Username, Email, LastLogin) VALUES ('Charlie', '1234@example.com', NOW());
+INSERT INTO Users (Username, LastLogin) VALUES ('John', NOW());
+INSERT INTO Users (Username, LastLogin) VALUES ('Alice', '2020-01-30 09:45:00-05');
+INSERT INTO Users (Username, LastLogin) VALUES ('Bob', '2020-02-01 15:05:00-08');
+INSERT INTO Users (Username, LastLogin) VALUES ('Charlie', NOW());
 
-INSERT INTO ALarmLog (UserID, AlarmStart, AlarmTimeout) VALUES (1, NOW() - INTERVAL '2 minutes', NOW());
-INSERT INTO ALarmLog (UserID, AlarmStart, AlarmTimeout) VALUES (3, '2020-02-01 18:25:00-08', '2020-02-01 18:27:00-08');
+INSERT INTO ALarmLog (UserID, AlarmStart, AlarmEnd, AlarmSent) VALUES (1, NOW() - INTERVAL '2 minutes', NOW(), TRUE);
+INSERT INTO ALarmLog (UserID, AlarmStart, AlarmEnd, AlarmSent) VALUES (3, '2020-02-01 18:25:00-08', '2020-02-01 18:27:00-08', TRUE);
+INSERT INTO ALarmLog (UserID, AlarmStart, AlarmEnd, AlarmSent) VALUES (2, NOW() - INTERVAL '2 minutes', NOW(), FALSE);
 
 INSERT INTO ResponseLog (ResponderID, AlarmID, AlertResponse, ResponseTime) VALUES (4, 1, FALSE, NOW());
 INSERT INTO ResponseLog (ResponderID, AlarmID, AlertResponse, ResponseTime) VALUES (3, 1, TRUE, NOW());
