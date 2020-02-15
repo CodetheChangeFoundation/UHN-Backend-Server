ab560ae8ada244b8e6ad8b2c1e78d35860eb3a09
Matthew Lee <matthewleemle@gmail.com>
2020-02-15 15:19:34 -0800

    change in schema

--- models/help_request.js
+++ models/help_request.js
@@ -2,22 +2,33 @@ var database = require("../database/database");
 var mongoose = database.getmongoose();
 var Schema = mongoose.Schema;
 
+const arrayLimit = (val) => {
+  return val.length <= 6;
+}
+
+
 const model = mongoose.model("help_requests", new Schema({
   userId: String,
-  responderId: String,
+  responderIds: {
+    [{ _id: false, id: String
+    }], // max of 6
+    validate: [arrayLimit, '{PATH} exceeds the limit of 6']
+  }
   status: {
     type: String,
     enum: ["open", "sent_to_responder", "taken", "arrived", "resolved"]
   },
   responders: [{ _id: false, id: String }]
 },
-{ 
-  timestamps: { 
-    createdAt: "createdAt", 
-    updatedAt: "updatedAt" 
-  }, 
-  versionKey: false 
+{
+  timestamps: {
+    createdAt: "createdAt",
+    updatedAt: "updatedAt"
+  },
+  versionKey: false
 }));
+
+
 
 module.exports = {
   model
