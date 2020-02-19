2476cb5aa1613124531b9ab6aa9dcf0ea7859fbd
andrchoi <andrew.tt.choi@gmail.com>
2020-02-18 21:31:51 -0800

    BS-28: converted boolean to string before use

--- controllers/metrics/response.js
+++ controllers/metrics/response.js
@@ -3,17 +3,19 @@ const handle = require("../../Utils/error_handling");
 
 async function recordResponse(req, res) {
   let data = req.body;
+  let response = req.body.response.toString();
 
   try {
-    let responseID = await responseService.createResponseLog(data.alarmID, data.userID, data.response, data.responseTime);
+    let responseID = await responseService.createResponseLog(data.alarmID, data.userID, response, data.responseTime);
     res.status(200).json({
       id: responseID,
       userID: data.userID,
       alarmID: data.alarmID,
-      response: data.response,
+      response: response,
       responseTime: data.responseTime
     });
   } catch (err) {
+    console.log(err)
     handle.internalServerError(res, "Cannot create response log");
   }
 }
