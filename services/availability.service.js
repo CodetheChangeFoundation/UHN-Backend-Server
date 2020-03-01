1ec0c2dbe5b417e5599513e417685c8def896e35
Asiapenguin <nicholaskong@live.ca>
2020-03-01 15:59:13 -0800

    Better redis

--- services/availability.service.js
+++ services/availability.service.js
@@ -1,26 +1,26 @@
 var redis = require("./redis");
-const availabilityValue = "available";
+const availableUsers = "available_users";
 
-async function setAvailable(userid) {
+async function setAvailable(userId) {
   try {
-    const res = await redis.setAsync(availabilityValue + userid, availabilityValue);
+    const res = await redis.saddAsync(availableUsers, userId);
   } catch(err) {
     console.log("redis setAvailable error: ", err.message);
   }
 }
 
-async function setUnavailable(userid) {
+async function setUnavailable(userId) {
   try {
-    const res = await redis.delAsync(availabilityValue + userid);
+    const res = await redis.sremAsync(availableUsers, userId);
   } catch(err) {
     console.log("redis setUnavailable error: ", err.message);
   }
 }
 
-async function checkAvailabilityStatus(userid) {
+async function checkAvailabilityStatus(userId) {
   try {
-    const res = await redis.getAsync(availabilityValue + userid);
-    return res === availabilityValue;
+    const res = await redis.sismemberAsync(availableUsers, userId);
+    return res ? true : false;
   } catch(err) {
     console.log("redis checkAvailabilityStatus error: ", err.message);
     return false;
