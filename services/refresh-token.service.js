31a6e717966e8f2bf7f1a34b8d2616f7ba7cbbb3
Asiapenguin <nicholaskong@live.ca>
2020-03-01 16:45:06 -0800

    Switch key value

--- services/refresh-token.service.js
+++ services/refresh-token.service.js
@@ -3,7 +3,7 @@ const refreshTokens = "refresh_tokens";
 
 async function addRefreshToken(userId, refreshToken) {
   try {
-    const res = await redis.hsetAsync(refreshTokens, userId, refreshToken);
+    const res = await redis.hsetAsync(refreshTokens, refreshToken, userId);
   } catch(err) {
     console.log("redis addRefreshToken error: ", err.message);
   }
