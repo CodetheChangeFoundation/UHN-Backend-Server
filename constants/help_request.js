c7529328433c897e45d00640dd6146582f34d9fd
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-03-07 14:14:03 -0800

    clean bugs on putHelpRequest

---
+++ constants/help_request.js
@@ -0,0 +1,7 @@
+const HELP_REQUEST_STATUS = ["open", "sent_to_responder", "taken", "arrived", "resolved", null]
+const HELP_REQUEST_LIMIT = 6
+
+module.exports = {
+    HELP_REQUEST_STATUS,
+    HELP_REQUEST_LIMIT
+}
--- controllers/help_request.js
+++ controllers/help_request.js
@@ -4,64 +4,56 @@ var HelpRequestModel = require("../models/help_request").model;
 var NotificationService = require("../services/notification.service");
 var UserService = require("../services/user.service");
 import * as StatusCodes from "../utils/error_status_codes"
+const { HELP_REQUEST_STATUS, HELP_REQUEST_LIMIT } = require('../constants/help_request')
 
 const putHelpRequest = async (req, res) => {
-  let status = req.body.status;
-  let newResponderId = req.body.newResponderId;
-  let helpReqId = req.params.id;
+  const status = req.body.status;
+  const newResponderId = req.body.newResponderId;
+  const helpReqId = req.params.id;
 
-  if ((!(status === "open" || status === "sent_to_responder" || status === "taken" || status === "arrived" || status === "resolved" || status == null)) || (!newResponderId && !status)) {
-    handle.badRequest(res, "Incorrect request", StatusCodes.fieldError);
+  if (!HELP_REQUEST_STATUS.some((hr_status) => hr_status === status ) && !newResponderId) {
+    handle.badRequest(res, "Incorrect help request - one or multiple required fields are missing", StatusCodes.fieldError);
   }
   else {
     try {
       var help_request = await HelpRequestModel.findOne({
         _id: new ObjectId(helpReqId)
       })
-    } catch (err) {
-      handle.internalServerError(res, err.message);
-    }
 
-    if (help_request == null)
-      handle.notFound(res, "Help Request does not exist")
-    else {
-      let responderIds = help_request.responderIds;
-      if (responderIds.some(r => r.id === newResponderId))
-        handle.badRequest(res, "Responder has already been added", StatusCodes.duplicateError);
-      else {
-        let limitReached = false;
-        try {
-          if(newResponderId){
-            help_request.responderIds.push({ _id: false, id: newResponderId });
-            await help_request.save();
-        }
-        }
-        catch (err) {
-          limitReached = true
-          handle.badRequest(res, err.message, StatusCodes.limitReachedError);
-        }
+      // If the helpRequedId does not exist
+      if(help_request === null) throw "NOT_FOUND"
 
-        if (!limitReached) {
-          if(status){
-              help_request.status = status;
-              try{
-                await help_request.save();
-              }
-              catch(err){
-                    handle.internalServerError(res, err.message);
-              }
-          }
-          res.status(200).json({
-            id: help_request._id.toString(),
-            userId: help_request.userId,
-            responderIds: help_request.responderIds,
-            status: help_request.status,
-            userResponders: help_request.userResponders,
-            createdAt: help_request.createdAt,
-            updatedAt: help_request.updatedAt
-          });
-        }
+      if(newResponderId) {
+        let responderIds = help_request.responderIds;
+
+        // If newResponderID already exists
+        if(responderIds.some(r => r.id === newResponderId)) throw "DUPLICATE"
+
+        // If responderID limit reached and there is no status update
+        if(help_request.responderIds.length >= HELP_REQUEST_LIMIT && !status) throw "LIMIT"
+        else help_request.responderIds.push({ _id: false, id: newResponderId })
       }
+
+      if(status) help_request.status = status;
+
+      await help_request.save();
+
+      res.status(200).json({
+        id: help_request._id.toString(),
+        userId: help_request.userId,
+        responderIds: help_request.responderIds,
+        status: help_request.status,
+        // unsure if we need 'userResponders' still
+        userResponders: help_request.userResponders,
+        createdAt: help_request.createdAt,
+        updatedAt: help_request.updatedAt
+      });
+
+    } catch (err) {
+      if(err === "NOT_FOUND") handle.notFound(res, "Help Request does not exist")
+      else if(err === "DUPLICATE") handle.badRequest(res, "Responder has already been added", StatusCodes.duplicateError);
+      else if(err === "LIMIT") handle.badRequest(res, err.message, StatusCodes.limitReachedError);
+      else handle.internalServerError(res, err.message);
     }
   }
 }
