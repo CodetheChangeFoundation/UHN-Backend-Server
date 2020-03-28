1a2b8e6d5f51bf4db27277c56c86d22eb1fdda92
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-03-28 15:45:51 -0700

    remove email from user schema and all validations

--- utils/error_messages.js
+++ utils/error_messages.js
@@ -2,9 +2,6 @@
 const USERNAME_MANDATORY = "Username is a mandatory field";
 const USERNAME_CONDITION = "Username must contain at least 5 characters";
 
-const EMAIL_MANDATORY = "Email is a mandatory field";
-const EMAIL_CONDITION = "Email is invalid";
-
 const PASSWORD_MANDATORY = "Password is a mandatory field";
 const PASSWORD_CONDITION = "Password must contain at least 5 characters";
 
@@ -13,8 +10,6 @@ const PHONE_MANDATORY = "Phone number is a mandatory field";
 module.exports = {
   USERNAME_MANDATORY,
   USERNAME_CONDITION,
-  EMAIL_MANDATORY,
-  EMAIL_CONDITION,
   PASSWORD_MANDATORY,
   PASSWORD_CONDITION,
   PHONE_MANDATORY
