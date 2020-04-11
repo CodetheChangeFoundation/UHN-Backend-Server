3386c76b2e9ce41baa57f230a14b80fb02a8a8fc
Matthew Lee <matthewleemle@gmail.com>
2020-04-10 20:48:02 -0700

    added lat lng threshold for get responder endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -128,8 +128,10 @@ async function userInfo(req, res) {
   result.onlineStatus = onlineStatus;
   res.status(200).json(result);
 }
-
 async function getResponders(req, res) {
+  var user_lat = req.body.lat;
+  var user_lng = req.body.lng;
+
   const user = await UserModel.findOne({
     _id: new ObjectId(req.params.id)
   }).lean();
@@ -142,10 +144,13 @@ async function getResponders(req, res) {
         _id: new ObjectId(r.id)
       }).lean();
       let availbilityStatus = await AvailbilityService.checkAvailabilityStatus(r.id);
+      let responder_coords = responder.location.coords;
+      let distance = distance(user_lat,user_lng,responder_coords.lat,responder_coords.lng,"K");
+
       returnInfo.push({
         id: r.id,
         username: responder.username,
-        availbilityStatus: availbilityStatus
+        availbilityStatus: (availbilityStatus&&(distance<=0.5))
       });
     }
     res.status(200).json({ responders: returnInfo });
@@ -154,6 +159,27 @@ async function getResponders(req, res) {
   }
 }
 
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
 async function getResponderCount(req, res) {
   const user = await UserModel.findOne({
     _id: new ObjectId(req.params.id)
