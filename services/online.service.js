58987b84dc1fe82305de845543dd366675a6960e
Asiapenguin <nicholaskong@live.ca>
2020-05-14 13:38:46 -0700

    Fix warning on deprecation toString on redis set commands

--- services/online.service.js
+++ services/online.service.js
@@ -3,7 +3,7 @@ const onlineUsers = "online_users"
 
 async function setOnline(userId) {
   try {
-    await redis.saddAsync(onlineUsers, userId);
+    await redis.saddAsync(onlineUsers, userId.toString());
   } catch(err) {
     console.error("redis setOnline error: ", err.message);
   }
@@ -11,7 +11,7 @@ async function setOnline(userId) {
 
 async function setOffline(userId) {
   try {
-    await redis.sremAsync(onlineUsers, userId);
+    await redis.sremAsync(onlineUsers, userId.toString());
   } catch(err) {
     console.error("redis setOffline error: ", err.message);
   }
@@ -19,7 +19,7 @@ async function setOffline(userId) {
 
 async function checkOnlineStatus(userId) {
   try {
-    const res = await redis.sismemberAsync(onlineUsers, userId);
+    const res = await redis.sismemberAsync(onlineUsers, userId.toString());
     return res ? true : false;
   } catch(err) {
     console.error("redis checkOnlineStatus error: ", err.message);
